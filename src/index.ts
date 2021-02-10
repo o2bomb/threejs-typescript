65bd680ed5ff858ad89e63d436e12e0d51df47ab
felix t <41817193+o2bomb@users.noreply.github.com>
2021-02-10 10:54:23 +0800

    Refactor code

--- src/index.ts
+++ src/index.ts
@@ -4,77 +4,64 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import "./style.css";
 
 window.addEventListener("load", init, false);
-window.addEventListener("resize", handleWindowResize, false);
-// document.addEventListener("mousemove", handleMouseMove, false);
 
 let WIDTH = window.innerWidth;
 let HEIGHT = window.innerHeight;
-let scene: THREE.Scene, camera: THREE.PerspectiveCamera, renderer: THREE.Renderer;
-let cube: THREE.Mesh;
-let orbitControls: OrbitControls;
 
 function init() {
-  scene = createScene();
-  camera = createCamera();
-  renderer = createRenderer();
+  // document.addEventListener("mousemove", handleMouseMove, false);
+  window.addEventListener(
+    "resize",
+    () => {
+      WIDTH = window.innerWidth;
+      HEIGHT = window.innerHeight;
 
-  // CONTROLS
-  orbitControls = new OrbitControls(camera, renderer.domElement);
-  orbitControls.enableDamping = true;
+      renderer.setSize(WIDTH, HEIGHT);
+      camera.aspect = WIDTH / HEIGHT;
+      camera.updateProjectionMatrix();
+    },
+    false
+  );
 
-  // CUBE
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshNormalMaterial();
-  cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-
-  animate();
-}
-
-function handleWindowResize() {
-  WIDTH = window.innerWidth;
-  HEIGHT = window.innerHeight;
-
-  renderer.setSize(WIDTH, HEIGHT);
-  camera.aspect = WIDTH / HEIGHT;
-  camera.updateProjectionMatrix();
-}
-
-function animate() {
-  requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render(scene, camera);
-}
-
-function createScene() {
+  // SCENE
   const scene = new THREE.Scene();
 
-  return scene;
-}
-
-function createCamera() {
+  // CAMERA
   const aspectRatio = window.innerWidth / window.innerHeight;
   const fieldOfView = 60;
   const nearPlane = 1;
-  const farPlane = 10000;
+  const farPlane = 1000;
   const camera = new THREE.PerspectiveCamera(
     fieldOfView,
     aspectRatio,
     nearPlane,
     farPlane
   );
-
   camera.position.z = 5;
-  return camera;
-}
 
-function createRenderer() {
+  // RENDERER
   const canvas = document.querySelector<HTMLCanvasElement>("#c");
   const renderer = new THREE.WebGLRenderer({ canvas });
   renderer.setSize(window.innerWidth, window.innerHeight);
 
-  return renderer;
+  // CONTROLS
+  const orbitControls = new OrbitControls(camera, renderer.domElement);
+  orbitControls.enableDamping = true;
+
+  // CUBE
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshNormalMaterial();
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+
+    orbitControls.update();
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+  };
+  animate();
 }
